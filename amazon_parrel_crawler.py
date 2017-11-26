637b4c90eee7d89d26003e68ae3610b84c34246b
yong mao <yyyuyong119@gmail.com>
2017-11-26 15:24:18 +0800

    final crawler for amazon

--- amazon_parrel_crawler.py
+++ amazon_parrel_crawler.py
@@ -31,20 +31,29 @@ def get_page(page):
 
 def get_all_book_links(soup):
 
-    links = []
+    global count
+
     for i in soup.findAll('li', {'class': 's-result-item celwidget '}):
-        book_tag = i.find('a', {'class': 'a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal'})
-        book_page = book_tag.get('href', '')
-        cnt += 1
-        links.append(book_page)
-    return links
+        try:
+            book_tag = i.find('a', {'class': 'a-link-normal a-text-normal'})
+            book_page = book_tag.get('href', '')
+            print 'HERE IS HREF: ', book_page
+            count += 1
+            print '#COUNT: ', count
+            add_page_to_folder(book_page)
+            print '&Add to file: ', book_page, 'DONE'
+        except:
+            pass
             
 def get_next_page(soup):
 
     next_page = ''
-    bottom_tag = soup.find('div', {'class': 'pagnHy'})
-    next_page_tag = bottom_tag.find('a', {'class': 'pagnNext'})
-    nextpage = urlparse.urljoin(seed, next_page_tag.get('href', ''))
+    try:
+        bottom_tag = soup.find('div', {'class': 'pagnHy'})
+        next_page_tag = bottom_tag.find('a', {'class': 'pagnNext'})
+        next_page = urlparse.urljoin(seed, next_page_tag.get('href', ''))
+    except:
+        next_page = ''
     return next_page
        
 def add_page_to_folder(page):
@@ -54,23 +63,26 @@ def add_page_to_folder(page):
     index_filename = 'amazon_index.txt'
     folder = 'amazon_html'
     try:
-        filename = str(cnt) + '.' + valid_filename(page)
+        filename = str(count) + '.' + valid_filename(page)
     except:
-        filename = str(cnt) + '.'
-
-    #Add page number and url into txt
-    index = open(index_filename, 'a')
-    index.write(str(cnt) + ' ' + page.encode('ascii', 'ignore') + '\n')
-    index.close()
+        filename = str(count) + '.'
 
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-    #Add page content into folder
-    f = open(os.path.join(folder, filename), 'w')
-    f.write(content)
-    f.close()
-    print '&Add to file: ', page, 'DONE'
+    try:
+        #Add page content into folder
+        f = open(os.path.join(folder, filename), 'w')
+        f.write(content)
+        f.close()
+
+        #Add page number and url into txt
+        index = open(index_filename, 'a')
+        index.write(str(count) + ' ' + page.encode('ascii', 'ignore') + '\n')
+        index.close()
+    except:
+        pass
+
 
 def crawling():
 
@@ -81,23 +93,14 @@ def crawling():
         if page not in crawled:
             crawled.append(page)
             content = get_page(page)
-            print content
-            print '$$$$$$$$$$$$$$$$$$$$$$', page
             soup = BeautifulSoup(content, 'html.parser')
-            print '#', count, page
-
             #Add next page to queue if it exists
             tmp = get_next_page(soup)
             if tmp != '':
                 print '$next page: ', tmp
                 q.put(tmp)
-
             #Subtract the book page from list page
-            book_links = get_all_book_links(soup)
-
-            #Add the book page into folder and txt
-            for link in book_links:
-                add_page_to_folder(link)
+            get_all_book_links(soup)
 
 if __name__ == '__main__':
 
@@ -108,7 +111,6 @@ if __name__ == '__main__':
     q = Queue.Queue()
     content = get_page('https://www.amazon.cn/gp/book/all_category/')
     soup = BeautifulSoup(content, 'html.parser')
-
     #Get links of all categories
     for i in soup.findAll('div', {'class': 'a-column a-span9 a-text-center'}):
         for j in i.findAll('a', {'class': 'a-link-nav-icon'}):
@@ -116,7 +118,6 @@ if __name__ == '__main__':
             q.put(page)
             print page
     print 'getting categories done'
-
     #Start to get book page
     crawling()
 
