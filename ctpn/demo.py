c8bb91d4428a5086b8ed4d3e489800e41f2fe9c5
yyong <1479773600@qq.com>
2017-12-18 23:50:19 +0800

    change bounding box mode

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -23,9 +23,9 @@ def resize_im(im, scale, max_scale = None):
 def draw_boxes(img, image_name, boxes, scale):
     for box in boxes:
         if box[8] >= 0.9:
-            color = (0,255,0)
+            color = (0, 0, 255)
         elif box[8] >= 0.8:
-            color = (255,0,0)
+            color = (0, 255, 0)
         cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 2)
         cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 2)
         cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 2)
