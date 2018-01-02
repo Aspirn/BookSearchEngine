b58a1e323a12ea5e17142a834efc19b3b617378a
yyong119 <yyyuyong119@gmail.com>
2018-01-02 21:08:11 +0800

    完善信息

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ pip install easydict -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install keras==2.0.8 tensorflow==1.4 tensorflow-gpu==1.4 -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install Cython opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install matplotlib -i https://pypi.tuna.tsinghua.edu.cn/simple/
-conda install pytorch torchvision -c pytorch
+conda install pytorch torchvision -c pytorch ##需要科学上网
 cd ./ctpn/lib
 python setup.py build ##最好使用sudo运行
 
